00a6355d2de9525ad16320ec8385afbbfac26de8
michaelGonzalez1812 <michael.gonzalez1812@gmail.com>
2019-04-10 14:15:34 -0600

    Open write and close file

--- src/rsa.s
+++ src/rsa.s
@@ -13,9 +13,12 @@
 */
 .data
      .balign 8
-     greeting: .asciz "Hello world\n"
-     after_greeting:
-     .set size_of_greeting, after_greeting - greeting
+     fname: .asciz "/home/mikepi/rsa/keys"
+     after_fname:
+     .set size_of_fname, after_fname - fname
+
+     .balign 4
+     value: .word 4
 
 /*
           Code section
@@ -30,20 +33,36 @@ main:
      stp	x29, x30, [sp, -16]!
      add	x29, sp, 0
 
+     //openat call
      //syscall's arguments
-     mov x0, #1
-     ldr x1, addr_of_greeting
-     mov x2, #size_of_greeting
+     mov x0, -100
+     ldr x1, fname_addr
+     mov x2, 2
+     mov x8, 56 //syscall #
+     svc #0
+     mov x10, x0
 
-     mov x8, #64 //syscall #
+     ldr x1, value_addr
+     mov x2, 4
+     mov x8, 64 //syscall #
      svc #0
 
+     mov x0, x10
+     mov x8, 64 //syscall #
+
+
      mov	w0, 0
+     mov x8, 57 //syscall #
+     svc #0
+
      ldp	x29, x30, [sp], 16
      ret
 
 //refereces to variables in code section
-addr_of_greeting : .quad greeting
+fname_addr:
+     .quad fname
+value_addr:
+     .quad value
 
 .size	main, .-main
 .ident	"GCC: (Ubuntu/Linaro 7.3.0-27ubuntu1~18.04) 7.3.0"
