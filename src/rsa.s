f305c7516352a7bfbe4e7d7c0ddff2498920129a
michaelGonzalez1812 <michael.gonzalez1812@gmail.com>
2019-04-10 11:35:21 -0600

    General comments

--- src/rsa.s
+++ src/rsa.s
@@ -1,35 +1,48 @@
+/******************************************************************************
+ * RSA Algorithm implementation (124 bits key)
+ * Tecnologico de Costa Rica
+ * Arquitectura de computadores I
+ * Author: Michael Gonzalez Rivera
+ ******************************************************************************/
+
 .arch armv8-a
 .file	"test.c"
 
+/*
+          Data section
+*/
 .data
-.balign 8
+     .balign 8
+     greeting: .asciz "Hello world\n"
+     after_greeting:
+     .set size_of_greeting, after_greeting - greeting
 
-greeting: .asciz "Hello world\n"
-after_greeting:
-.set size_of_greeting, after_greeting - greeting
-
+/*
+          Code section
+*/
 .text
-
-.align	2
-.global	main
-.type	main, %function
+     .align	2
+     .global	main
+     .type	main, %function
 
 main:
 
      stp	x29, x30, [sp, -16]!
      add	x29, sp, 0
 
+     //syscall's arguments
      mov x0, #1
      ldr x1, addr_of_greeting
-     mov x2, 5
+     mov x2, #size_of_greeting
 
-     mov x8, #64
+     mov x8, #64 //syscall #
      svc #0
 
      mov	w0, 0
      ldp	x29, x30, [sp], 16
      ret
 
+//refereces to variables in code section
 addr_of_greeting : .quad greeting
 
 .size	main, .-main
