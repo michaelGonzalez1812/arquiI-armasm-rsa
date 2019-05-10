ba08ab854e0b13dcc899eab16653a8135e25fea2
michaelGonzalez1812 <michael.gonzalez1812@gmail.com>
2019-05-10 12:29:35 -0600

    Multiplication function

--- src/rsa.s
+++ src/rsa.s
@@ -8,9 +8,9 @@
 .arch armv8-a
 .file	"test.c"
 
-/*
-          Data section
-*/
+/******************************
+ *        Data section        *
+ ******************************/
 .data
 
      pfname: .asciz "/home/mikepi/rsa/pfile"
@@ -24,13 +24,42 @@
 
      .balign 16
      n: .octa 0 //variable n
-/*
-          Code section
-*/
+
+
+/******************************
+ *        Code section        *
+ ******************************/
 .text
      .align	2
      .global	main
      .type	main, %function
+
+/*-------------------------------
+     Multiplication method
+     128 bits arguments
+     -soruce1 x0 x2
+     -soruce2 x3 x4
+     return x0 x1 128bits number
+---------------------------------*/
+mul:
+     mul	 x7, x2, x0
+	umulh x4, x2, x0
+	madd	 x4, x3, x0, x4 //Multiply-add.
+	madd	 x4, x2, x1, x4
+	mov	 x0, x7
+	mov	 x1, x4
+     ret
+
+	.size	mul, .-mul
+	.align	2
+	.global	main
+	.type	main, %function
+
+/*-------------------------------
+     phi computation method
+     128 bits arguments
+     return x0 x1 128bits number
+---------------------------------*/
 
 main:
 
@@ -42,17 +71,17 @@ main:
      mov x0, -100
      ldr x1, pfname_addr
      mov x2, 2
-     mov x8, 56 //syscall #
+     mov x8, 56 //syscall __NR_openat
      svc #0
      mov x10, x0
 
      ldr x1, p_addr
      mov x2, 16
-     mov x8, 63
+     mov x8, 63 //syscall __NR_read
      svc #0
 
      mov x0, x10
-     mov x8, 57 //syscall #
+     mov x8, 57 //syscall __NR_close
      svc #0
 
      //openat call
@@ -60,31 +89,29 @@ main:
      mov x0, -100
      ldr x1, qfname_addr
      mov x2, 2
-     mov x8, 56 //syscall #
+     mov x8, 56 //syscall __NR_openat
      svc #0
      mov x10, x0
 
      ldr x1, q_addr
      mov x2, 16
-     mov x8, 63
+     mov x8, 63 //__NR_read
      svc #0
 
      mov x0, x10
-     mov x8, 57 //syscall #
+     mov x8, 57 //syscall __NR_close
      svc #0
 
      ldr x8, p_addr
      ldr x9, q_addr
      ldp	x2, x3, [x8]
 	ldp	x0, x1, [x9]
-	mul	x7, x2, x0
-	umulh x4, x2, x0
-	madd	x4, x3, x0, x4
-	madd	x4, x2, x1, x4
-	mov	x5, x7
-	mov	x6, x4
-     ldr x8, q_addr
-	stp	x5, x6, [x8]
+	
+     bl mul
+     
+     //Store n
+     ldr x8, n_addr
+	stp	x0, x1, [x8]
 
      mov	w0, 0
      ldp	x29, x30, [sp], 16
