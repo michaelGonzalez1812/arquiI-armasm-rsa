54b7dee6008d607a8997a06a6f20609743632788
michaelGonzalez1812 <michael.gonzalez1812@gmail.com>
2019-05-11 18:01:09 -0600

    phi computation

--- src/rsa.s
+++ src/rsa.s
@@ -25,6 +25,9 @@
      .balign 16
      n: .octa 0 //variable n
 
+     .balign 16
+     phi: .octa 0 //variable phi
+
 
 /******************************
  *        Code section        *
@@ -58,8 +61,45 @@ mul:
 /*-------------------------------
      phi computation method
      128 bits arguments
+     -p x0 x1
+     -q x0 x1
      return x0 x1 128bits number
 ---------------------------------*/
+phi_method:
+     sub	sp, sp, #32
+	stp	x0, x1, [sp, 16]
+	stp	x2, x3, [sp]
+	ldp	x2, x3, [sp, 16]
+     // p - 1
+	mov	x0, -1
+	mov	x1, -1
+	adds	x10, x2, x0
+	adc	x0, x3, x1
+	mov	x6, x10
+	mov	x7, x0
+	ldp	x2, x3, [sp]
+     //q - 1
+	mov	x0, -1
+	mov	x1, -1
+	adds	x10, x2, x0
+	adc	x0, x3, x1
+	mov	x4, x10
+	mov	x5, x0
+     //multiplication
+	mul	x1, x6, x4
+	umulh	x0, x6, x4
+	madd	x0, x7, x4, x0
+	madd	x0, x6, x5, x0
+	mov	x8, x1
+	mov	x9, x0
+	mov	x0, x8
+	mov	x1, x9
+	add	sp, sp, 32
+	ret
+	.size	phi_method, .-phi_method
+	.align	2
+	.global	main
+	.type	main, %function
 
 main:
 
@@ -102,22 +142,31 @@ main:
      mov x8, 57 //syscall __NR_close
      svc #0
 
+     //compute and store n
      ldr x8, p_addr
      ldr x9, q_addr
      ldp	x2, x3, [x8]
 	ldp	x0, x1, [x9]
-	
      bl mul
-     
-     //Store n
      ldr x8, n_addr
+	stp	x0, x1, [x8]
+
+     //compute and store phi
+     ldr x8, p_addr
+     ldr x9, q_addr
+     ldp	x2, x3, [x8]
+	ldp	x0, x1, [x9]
+     bl phi_method
+     ldr x8, phi_addr
 	stp	x0, x1, [x8]
 
      mov	w0, 0
      ldp	x29, x30, [sp], 16
      ret
 
-//refereces to variables in code section
+/********************************************
+ * refereces to variables in code section   *
+ ********************************************/
 pfname_addr:
      .quad pfname
 qfname_addr:
@@ -128,6 +177,8 @@ q_addr:
      .quad q
 n_addr:
      .quad n
+phi_addr:
+     .quad phi
 
 .size	main, .-main
 .ident	"GCC: (Ubuntu/Linaro 7.3.0-27ubuntu1~18.04) 7.3.0"
